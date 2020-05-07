d1a8a503519b9a544f45ec91622e3dba5b1fa307
Avinash Jojowar <avinashjojowar10@gmail.com>
2020-05-08 00:36:26 +0530

    index.html and script.js updated

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var css = document.querySelector("h3");
+var _ = require('lodash');
+
+console.log(_);
+
+var arr = [1, 2, 3, 4, 5, 6];
+
+console.log("answer : ", _.without(arr, 1));
+
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
