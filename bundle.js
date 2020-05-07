9a03da76c852dc77829d4fb19fc32a24690757a7
Avinash Jojowar <avinashjojowar10@gmail.com>
2020-05-08 00:41:38 +0530

    script.js & bundle.js updated

--- bundle.js
+++ bundle.js
@@ -17123,6 +17123,7 @@ var arr = [1, 2, 3, 4, 5, 6];
 
 console.log("answer : ", _.without(arr, 1));
 
+var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
